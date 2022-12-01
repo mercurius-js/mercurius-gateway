71f85ed72b0de55f16615170cd4ae3ccb660482f
Marco Ippolito <marcoippolito54@gmail.com>
2022-12-01 15:50:46 +0100

    feat: header collector

--- index.d.ts
+++ index.d.ts
@@ -75,6 +75,10 @@ export interface MercuriusServiceMetadata {
   name: string;
 }
 
+export interface Collectors {
+  collectHeaders?: boolean;
+}
+
 interface WsConnectionParams {
   connectionInitPayload?:
     | (() => Record<string, any> | Promise<Record<string, any>>)
@@ -108,6 +112,7 @@ export interface MercuriusGatewayService {
     | (() => WsConnectionParams | Promise<WsConnectionParams>)
     | WsConnectionParams;
   setResponseHeaders?: (reply: FastifyReply) => void;
+  collectors?: Collectors;
 }
 
 export interface MercuriusGatewayOptions {
---
+++ test/headers-collector.js
@@ -0,0 +1,215 @@
+'use strict'
+
+const { test } = require('tap')
+const Fastify = require('fastify')
+const { promisify } = require('util')
+const GQL = require('mercurius')
+const plugin = require('../index')
+const { buildFederationSchema } = require('@mercuriusjs/federation')
+const immediate = promisify(setImmediate)
+
+async function createTestService (t, schema, resolvers = {}, serviceName) {
+  const service = Fastify()
+
+  service.register(GQL, {
+    schema: buildFederationSchema(schema),
+    resolvers
+  })
+
+  service.addHook('onSend', async (request, reply, payload) => {
+    reply.header(serviceName, 'true')
+  })
+
+  await service.listen({ port: 0 })
+  return [service, service.server.address().port]
+}
+
+const users = {
+  u1: {
+    id: 'u1',
+    name: 'John'
+  },
+  u2: {
+    id: 'u2',
+    name: 'Jane'
+  }
+}
+
+const posts = {
+  p1: {
+    pid: 'p1',
+    title: 'Post 1',
+    content: 'Content 1',
+    authorId: 'u1'
+  },
+  p2: {
+    pid: 'p2',
+    title: 'Post 2',
+    content: 'Content 2',
+    authorId: 'u2'
+  },
+  p3: {
+    pid: 'p3',
+    title: 'Post 3',
+    content: 'Content 3',
+    authorId: 'u1'
+  },
+  p4: {
+    pid: 'p4',
+    title: 'Post 4',
+    content: 'Content 4',
+    authorId: 'u1'
+  }
+}
+
+const query = `
+    query {
+      me {
+        id
+        name
+        topPosts(count: 2) {
+          pid
+          author {
+            id
+          }
+        }
+      }
+      topPosts(count: 2) {
+        pid
+      }
+    }
+  `
+
+async function createTestGatewayServer (t, opts = {}) {
+  // User service
+  const userServiceSchema = `
+    type Query @extends {
+      me: User
+    }
+  
+    type User @key(fields: "id") {
+      id: ID!
+      name: String!
+    }`
+  const userServiceResolvers = {
+    Query: {
+      me: () => {
+        return users.u1
+      }
+    },
+    User: {
+      __resolveReference: (user) => {
+        return users[user.id]
+      }
+    }
+  }
+  const [userService, userServicePort] = await createTestService(
+    t,
+    userServiceSchema,
+    userServiceResolvers,
+    'user'
+  )
+
+  // Post service
+  const postServiceSchema = `
+    type Post @key(fields: "pid") {
+      pid: ID!
+      author: User
+    }
+  
+    extend type Query {
+      topPosts(count: Int): [Post]
+    }
+  
+    type User @key(fields: "id") @extends {
+      id: ID! @external
+      topPosts(count: Int!): [Post]
+    }`
+  const postServiceResolvers = {
+    Post: {
+      __resolveReference: (post) => {
+        return posts[post.pid]
+      },
+      author: (post) => {
+        return {
+          __typename: 'User',
+          id: post.authorId
+        }
+      }
+    },
+    User: {
+      topPosts: (user, { count }) => {
+        return Object.values(posts)
+          .filter((p) => p.authorId === user.id)
+          .slice(0, count)
+      }
+    },
+    Query: {
+      topPosts: (root, { count = 2 }) => Object.values(posts).slice(0, count)
+    }
+  }
+  const [postService, postServicePort] = await createTestService(
+    t,
+    postServiceSchema,
+    postServiceResolvers,
+    'post'
+  )
+
+  const gateway = Fastify()
+  t.teardown(async () => {
+    await gateway.close()
+    await userService.close()
+    await postService.close()
+  })
+
+  await gateway.register(plugin, {
+    gateway: {
+      services: [
+        {
+          name: 'user',
+          url: `http://localhost:${userServicePort}/graphql`,
+          collectors: {
+            collectHeaders: true
+          }
+        },
+        {
+          name: 'post',
+          url: `http://localhost:${postServicePort}/graphql`,
+          collectors: {
+            collectHeaders: true
+          }
+        }
+      ]
+    },
+    ...opts
+  })
+
+  return gateway
+}
+
+// -----
+// hooks
+// -----
+test('gateway - hooks', async (t) => {
+  t.plan(1)
+  const app = await createTestGatewayServer(t)
+
+  app.graphql.addHook('onResolution', async function (_, context) {
+    await immediate()
+    t.has(context.collectors.responseHeaders, {
+      topPosts: {
+        post: 'true'
+      },
+      me: {
+        user: 'true'
+      }
+    })
+  })
+
+  await app.inject({
+    method: 'POST',
+    headers: { 'content-type': 'application/json' },
+    url: '/graphql',
+    body: JSON.stringify({ query })
+  })
+})

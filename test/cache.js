f290a289baf56fd8ae791411e75d5b79242d9172
Luca Del Puppo <Puppo@users.noreply.github.com>
2025-12-17 06:56:43 +0100

    test: migrate to node:test and update assertions

--- test/cache.js
+++ test/cache.js
@@ -1,36 +1,31 @@
 'use strict'
 
-const { test } = require('tap')
+const { test } = require('node:test')
 const { buildCache } = require('../lib/util')
 
 test('buildCache: default ', async t => {
-  t.plan(1)
   const cache = buildCache({})
-  t.same(cache.max, 1024)
+  t.assert.deepStrictEqual(cache.max, 1024)
 })
 
 test('buildCache: with a boolean ', async t => {
-  t.plan(1)
   const cache = buildCache({ cache: true })
-  t.same(cache.max, 1024)
+  t.assert.deepStrictEqual(cache.max, 1024)
 })
 
 test('buildCache: disabled with a boolean ', async t => {
-  t.plan(1)
   const cache = buildCache({ cache: false })
-  t.notOk(cache)
+  t.assert.ok(!cache)
 })
 
 test('buildCache: with an integer', async t => {
-  t.plan(1)
   const cache = buildCache({ cache: 10 })
-  t.same(cache.max, 10)
+  t.assert.deepStrictEqual(cache.max, 10)
 })
 test('buildCache: with an integer', async t => {
-  t.plan(1)
   try {
     buildCache({ cache: 'wrong value' })
   } catch (error) {
-    t.same(error.message, 'Invalid options: Cache type is not supported')
+    t.assert.strictEqual(error.message, 'Invalid options: Cache type is not supported')
   }
 })
